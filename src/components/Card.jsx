961d0bbef9c9cb74e7a8225340048b9184f9f61e
geroxima <geroshima123@gmail.com>
2024-07-20 16:25:00 -0400

    refactor: Update Card component styles for improved readability and responsiveness

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,10 +9,10 @@ const Card = ({ nombre, tipo, direccion, descripcion, onClick }) => {
   };
 
   return (
-    <div className=" rounded overflow-hidden shadow-md  shadow-[#B2F3BB] w-full h-fit cursor-pointer" onClick={onClick}>
+    <div className=" rounded overflow-hidden border border-slate-200 w-full h-fit cursor-pointer" onClick={onClick}>
       <div className="px-6 py-4">
         <div className="font-bold text-sm  text-slate-500">{nombre}</div>
-        <div className="font-bold text-xs mb-2 text-slate-300">{tipo}</div>
+        <div className="font-bold text-xs mb-2 text-slate-400">{tipo}</div>
         <p className="text-gray-700 text-xs">
           {isExpanded ? descripcion : `${descripcion.slice(0, maxLength)}...`}
         </p>

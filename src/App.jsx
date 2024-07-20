bf895b27c87573229796719edf21fa9c9fa8a76d
geroxima <geroshima123@gmail.com>
2024-07-20 13:50:32 -0400

    feat: Update Chat component to remove unnecessary LoaderModal prop and add SendIcon component

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,13 @@ import LoaderModal from './components/LoaderModal.jsx';
 
 const App = () => {
   let [places, setPlaces] = useState([]);
-  let [isLoading, setisLoading] = useState(false);
 
 
   return (
     <>
       <div className="w-full h-full m-0 p-3 bg-slate-50 flex">
-        <LoaderModal isOpen={isLoading} text="Cargando" />
         <MapComponent locations={places} className={`hidden`} />
-        <Chat setPlaces={setPlaces} setOnload={setisLoading} />
+        <Chat setPlaces={setPlaces}/>
       </div>
     </>
   );
--- src/components/LoaderModal.jsx
+++ src/components/LoaderModal.jsx
@@ -1,19 +1,19 @@
-const LoaderModal = ({ isOpen, text ="Cargando" }) => {
+const LoaderModal = ({ isOpen}) => {
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div className="absolute top-0 left-0 z-50 w-screen h-screen flex items-center justify-center bg-slate-500/30">
+    <div className="items-center justify-center bg-slate-500/30">
       <div className="bg-white border py-2 px-5 rounded-lg flex items-center flex-col">
-        <div className="loader-dots block relative w-20 h-5 mt-2">
-          <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-sky-500"></div>
-          <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-sky-500"></div>
-          <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-sky-500"></div>
-          <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-sky-500"></div>
-        </div>
-        <div className="text-gray-500 text-xs font-medium mt-2 text-center">
-          {text}
+        <div role="status" class="max-w-sm animate-pulse">
+            <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+            <span class="sr-only">Loading...</span>
         </div>
       </div>
     </div>
